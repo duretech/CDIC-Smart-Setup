6f921908c64f01bb0e156aa726f967129b572e25
SumedhAtDure <sumedh.dixit@duretechnologies.com>
2026-01-30 12:52:00 +0530

    basicAuthToken

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -14,6 +14,7 @@ import imgurl from '../assets/images/imgUrl';
 
 import { useSelector, useDispatch } from 'react-redux';
 import { setLoader } from '../redux/actions/userAction'
+import { basicAuthToken } from '../config/appConfig';
 
 const Register = () => {
     const history = useHistory()
